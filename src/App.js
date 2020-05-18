4268b4fb38feec4882a7ae4a9d86191cbc8a4fd4
Vinh Nguyen <developerthienkim@gmail.com>
2020-05-18 23:17:54 +0700

    Make todo list for todo app

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ul>
+          <li>Go to market</li>
+          <li>Buy food</li>
+          <li>Make dinner</li>
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
